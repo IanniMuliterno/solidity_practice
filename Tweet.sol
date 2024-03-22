23ea57f738816bf3de5bf5efcb2ca47c1026925e
Ían Muliterno <iannimuliterno@gmail.com>
2024-03-22 15:25:13 -0300

    implemented array

--- Tweet.sol
+++ Tweet.sol
@@ -3,15 +3,20 @@ pragma solidity 0.8.24;
 
 contract Tweet {
 
-    mapping (address => string) public tweets;
+    mapping (address => string[]) public tweets;
 
     function creatTweet(string memory _tweet) public {
 
-        tweets[msg.sender] = _tweet;
+        tweets[msg.sender].push(_tweet);
     }
 
     
-    function getTweet (address _owner) public view  returns (string memory) {
-    return tweets[_owner];
+    function getTweet (address _owner,uint _i) public view returns (string memory) {
+    return tweets[_owner][_i];
     }
+
+    function getalltweets(address _owner) public view returns (string[] memory) {
+        return tweets[_owner];
+    }
+
 }
