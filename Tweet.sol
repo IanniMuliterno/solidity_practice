d2ba3395acf8bdf415b07dd552f86b76a26dd461
Ían Muliterno <iannimuliterno@gmail.com>
2024-03-23 17:06:59 -0300

    upgrading tweet to struct
    now we can identify a set of attributes in tweet, just like a TS `class`

--- Tweet.sol
+++ Tweet.sol
@@ -1,21 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-contract Tweet {
+contract Twitter {
 
-    mapping (address => string[]) public tweets;
+    struct Tweet {
+        address author;
+        string content;
+        uint256 timestamp;
+        uint256 likes;
+     }
+
+    mapping (address => Tweet[]) public tweets;
 
     function creatTweet(string memory _tweet) public {
 
-        tweets[msg.sender].push(_tweet);
+        Tweet memory newTweet = Tweet({
+            author: msg.sender,
+            content: _tweet,
+            timestamp: block.timestamp,
+            likes: 0
+        });
+
+        tweets[msg.sender].push(newTweet);
     }
 
     
-    function getTweet (address _owner,uint _i) public view returns (string memory) {
+    function getTweet (address _owner,uint _i) public view returns (Tweet memory) {
     return tweets[_owner][_i];
     }
 
-    function getalltweets(address _owner) public view returns (string[] memory) {
+    function getalltweets(address _owner) public view returns (Tweet[] memory) {
         return tweets[_owner];
     }
 
