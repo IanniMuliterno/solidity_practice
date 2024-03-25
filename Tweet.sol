d0376e8d70d98c01d391f4dded1302b4cb717e63
Ían Muliterno <iannimuliterno@gmail.com>
2024-03-24 21:51:22 -0300

    using msg.sender as input and modifier exercise
    instead of asking for the user to pass a owner.
    
    implemented `onlyOwner` modifier

--- Tweet.sol
+++ Tweet.sol
@@ -3,6 +3,9 @@ pragma solidity 0.8.24;
 
 contract Twitter {
 
+    uint16 constant max_length = 280;
+
+
     struct Tweet {
         address author;
         string content;
@@ -10,9 +13,24 @@ contract Twitter {
         uint256 likes;
      }
 
+           constructor() {
+        owner = msg.sender;
+     };   
+
+     modifier onlyOwner {
+        require(msg.sender == owner, "you are not the owner");
+        _;
+     }
+
+     function changeName(string newOwner) public onlyOwner {
+        owner = newOwner;
+     }
+
     mapping (address => Tweet[]) public tweets;
 
     function creatTweet(string memory _tweet) public {
+
+        require(bytes(_tweet).length <= max_length,"twitter is too long");
 
         Tweet memory newTweet = Tweet({
             author: msg.sender,
@@ -25,12 +43,12 @@ contract Twitter {
     }
 
     
-    function getTweet (address _owner,uint _i) public view returns (Tweet memory) {
-    return tweets[_owner][_i];
+    function getTweet (uint _i) public view returns (Tweet memory) {
+    return tweets[msg.sender][_i];
     }
 
-    function getalltweets(address _owner) public view returns (Tweet[] memory) {
-        return tweets[_owner];
+    function getalltweets() public view returns (Tweet[] memory) {
+        return tweets[msg.sender];
     }
 
 }
