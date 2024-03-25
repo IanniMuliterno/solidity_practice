5982caa321aad15cb8789386414bc1fd572ec4de
Ían Muliterno <iannimuliterno@gmail.com>
2024-03-25 20:03:15 -0300

    starting `pausableToken` contract
    new toy contract, I started by declaring the intuition, the next step is doing by myself from scratch, aiming to depend only in the documentation if needed be.

---
+++ pausableToken.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+// intuition
+// 1. start a contract that is pausable and alows transfer
+// 2. declare address owner, bool paused and mapping balance
+// 3. declare constructor
+// 4. create a onlyOwner and notpaused modifier
+// 5. create functions pause and unpause denpending on onlyOwner
+// 6. create a transfer function that depends on notpaused modifier
+
+pragma solidity ^0.8.24;
+
+contract pausableToken {
+
+
+
+}
